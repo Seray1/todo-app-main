c2928172183b1dd8462f1ff31affaf6140345c44
Seray1 <107807594+Seray1@users.noreply.github.com>
2023-06-30 00:51:39 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        VeryLightGray: "hsl(0, 0%, 98%)",
+        VeryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        LightGrayishBlue: "hsl(233, 11%, 84%)",
+        DarkGrayishBlue: "hsl(236, 9%, 61%)",
+        VeryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+        VeryDarkBlue: "hsl(235, 21%, 11%)",
+        VeryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        LightGrayishBlue: "hsl(234, 39%, 85%)",
+        LightGrayishBlueHover: "hsl(236, 33%, 92%)",
+        DarkGrayishBlue: "hsl(234, 11%, 52%)",
+        VeryDarkGrayishBlue1: "hsl(233, 14%, 35%)",
+        VeryDarkGrayishBlue2: "hsl(237, 14%, 26%)",
+        BrightBlue: "hsl(220, 98%, 61%)",
+        CheckBackground:
+          "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+      },
+    },
+  },
+  plugins: [],
+}
